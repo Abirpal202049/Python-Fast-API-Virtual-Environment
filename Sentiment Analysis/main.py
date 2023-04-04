c39b619a8659476cfe3092e755c06c2babff062e
Abir Pal <abirpal282002@gmail.com>
2023-04-04 19:30:11 +0530

    Final Dep Changes

--- Sentiment Analysis/main.py
+++ Sentiment Analysis/main.py
@@ -1,39 +1,64 @@
 from fastapi import FastAPI
-import pickle
-import nltk
-# nltk.download('vader_lexicon')
-# !You need to download vader_lexicon using [python3 -m nltk.downloader vader_lexicon] to run the app
-# * https://rstudio-pubs-static.s3.amazonaws.com/591860_85c76c963bd84f25a197b8438256da9b.html
+from fastapi.middleware.cors import CORSMiddleware
 
-from tqdm.notebook import tqdm
+# Package to read the extracted ML model
+import pickle
+
+# All the Natural Language Processes
+import nltk
+# ! nltk.download('vader_lexicon')
+# ! You need to download vader_lexicon using [python3 -m nltk.downloader vader_lexicon] to run the app
+# ! https://rstudio-pubs-static.s3.amazonaws.com/591860_85c76c963bd84f25a197b8438256da9b.html
 from nltk.sentiment import SentimentIntensityAnalyzer
 from transformers import AutoTokenizer
 from transformers import AutoModelForSequenceClassification
 from scipy.special import softmax
 
+# For the progress bar
+from tqdm.notebook import tqdm
 
+
+# FastAPI app and cors policy 
 app = FastAPI()
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# Reading the .pkl file for extraction of the ML Model
 with open('TwitterSentimentModel', 'rb') as f:
   myModel = pickle.load(f)
 
+# Segrigating the Algo
 knn = myModel['knn']
-lr = myModel['lr'] # Logistic Regression
+lr = myModel['lr']
 rf = myModel['rf']
 mlr = myModel['mlr']
 
+
+# Natural Language Initilization
 sia = SentimentIntensityAnalyzer()
 task='sentiment'
 MODEL = f"cardiffnlp/twitter-roberta-base-{task}"
 tokenizer = AutoTokenizer.from_pretrained(MODEL)
 # ! You also need pytorch for AutoModelForSequenceClassification
-# * https://pytorch.org/
+# ! https://pytorch.org/
 model = AutoModelForSequenceClassification.from_pretrained(MODEL)
 
-
-@app.get('/home')
+# Test Route
+@app.get('/')
 def home_route():
     return {"message" : "Your backend server is up and running...", "success" : True}
 
+# Prediction Route
 @app.post('/predict')
 def predict_route(text: str):
     vadarScore = sia.polarity_scores(text)
